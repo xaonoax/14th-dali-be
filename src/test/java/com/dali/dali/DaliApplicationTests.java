b9470dc22eebf7e43218e0ab3a0fc95dd98f568c
DanteRoyal <dwroyal02@naver.com>
2023-08-24 21:31:36 +0900

    project sets

---
+++ src/test/java/com/dali/dali/DaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dali.dali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
