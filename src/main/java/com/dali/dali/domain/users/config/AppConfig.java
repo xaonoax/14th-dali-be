76d0dc94644349bb7877c7bacad3b51e3dc1cca2
Ji-0 <xaonoax@gmail.com>
2023-08-26 04:42:11 +0900

    Feat: OAuth2 네이버 로그인 기능 구현

---
+++ src/main/java/com/dali/dali/domain/users/config/AppConfig.java
@@ -0,0 +1,21 @@
+package com.dali.dali.domain.users.config;
+
+import org.json.simple.parser.JSONParser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@Component
+public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public JSONParser jsonParser() {
+        return new JSONParser();
+    }
+}
