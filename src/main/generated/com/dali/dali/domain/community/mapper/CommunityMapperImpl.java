5c428d7c46937c3ff16db3f26191dfe262236ed1
jjaei <ieajnowk@gmail.com>
2023-08-25 18:02:43 +0900

    Refactor: 엔티티 애노테이션 추가

--- src/main/generated/com/dali/dali/domain/community/mapper/CommunityMapperImpl.java
+++ src/main/generated/com/dali/dali/domain/community/mapper/CommunityMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-08-25T17:33:32+0900",
+    date = "2023-08-25T17:41:59+0900",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.7 (Oracle Corporation)"
 )
 @Component
