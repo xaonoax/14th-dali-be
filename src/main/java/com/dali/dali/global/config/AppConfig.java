594d8e7ec7cc5d5554e03a4293fb5e5a412a14b7
Ji-0 <xaonoax@gmail.com>
2023-08-26 18:16:34 +0900

    Chore: AppConfig 병합

--- src/main/java/com/dali/dali/global/config/AppConfig.java
+++ src/main/java/com/dali/dali/global/config/AppConfig.java
@@ -3,8 +3,10 @@ package com.dali.dali.global.config;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.parser.JSONParser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @RequiredArgsConstructor
@@ -14,6 +16,16 @@ public class AppConfig {
     @Bean
     public JPAQueryFactory queryFactory() {
         return new JPAQueryFactory(em);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public JSONParser jsonParser() {
+        return new JSONParser();
     }
 
 }
