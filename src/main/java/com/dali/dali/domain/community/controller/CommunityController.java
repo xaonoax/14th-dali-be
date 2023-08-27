56889c4a459a80c90b6028a72ab178ce7e4f8b7e
jjaei <ieajnowk@gmail.com>
2023-08-28 04:06:07 +0900

    Feat: 유저 로그인 연동 및 및 예외처리

--- src/main/java/com/dali/dali/domain/community/controller/CommunityController.java
+++ src/main/java/com/dali/dali/domain/community/controller/CommunityController.java
@@ -6,42 +6,49 @@ import com.dali.dali.domain.community.entity.Community;
 import com.dali.dali.domain.community.entity.Gender;
 import com.dali.dali.domain.community.entity.Time;
 import com.dali.dali.domain.community.service.CommunityService;
+import com.dali.dali.domain.users.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/communities")
 public class CommunityController {
     private final CommunityService communityService;
+    private final UserRepository userRepository;
 
     @PostMapping
-    public Community createPost(@RequestBody CommunityDto communityDto) {
-        return communityService.createPost(communityDto);
+    public Community createPost(@RequestBody CommunityDto communityDto, Principal principal) {
+        return communityService.createPost(communityDto, principal);
     }
 
     @GetMapping("/{id}")
-    public Community getPost(@PathVariable Long id) {
-        return communityService.getPost(id);
+    public Community getPost(@PathVariable Long id, Principal principal) {
+        return communityService.getPost(id, principal);
     }
 
     @GetMapping
     public Page<Community> getPosts(Pageable pageable,
                                     @RequestParam(value = "ampm", required = false) AMPM ampm,
                                     @RequestParam(value = "time", required = false) Time time,
-                                    @RequestParam(value = "gender", required = false) Gender gender, @RequestParam(value = "park", required = false) String park_name) {
-        return communityService.getPosts(pageable, ampm, time, gender, park_name);
+                                    @RequestParam(value = "gender", required = false) Gender gender,
+                                    @RequestParam(value = "park", required = false) String park_name,
+                                    Principal principal) {
+
+        return communityService.getPosts(pageable, ampm, time, gender, park_name, principal);
     }
 
     @PutMapping("/{id}")
-    public Community updatePost(@PathVariable Long id, @RequestBody CommunityDto communityDto) {
-        return communityService.updatePost(id, communityDto);
+    public Community updatePost(@PathVariable Long id, @RequestBody CommunityDto communityDto, Principal principal) {
+        return communityService.updatePost(id, communityDto, principal);
     }
 
     @DeleteMapping("{id}")
-    public Community deletePost(@PathVariable Long id) {
-        return communityService.deletePost(id);
+    public Community deletePost(@PathVariable Long id, Principal principal) {
+        return communityService.deletePost(id, principal);
     }
 }
