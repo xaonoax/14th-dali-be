5106059c5f8b1eb860b104c05cb29fa8dbdfc5d9
Ji-0 <xaonoax@gmail.com>
2023-08-31 05:13:47 +0900

    Refactor: 최대 레벨 수 조정

--- src/main/java/com/dali/dali/domain/users/repository/UserLevelRepositoryImpl.java
+++ src/main/java/com/dali/dali/domain/users/repository/UserLevelRepositoryImpl.java
@@ -15,9 +15,17 @@ public class UserLevelRepositoryImpl implements UserLevelRepository {
     @Override
     public void updateLevel(User user) {
         QUser qUser = QUser.user;
-        jq.update(qUser)
-                .set(qUser.level, qUser.level.add(1))
+
+        Integer countLevel = jq.select(qUser.level)
+                .from(qUser)
                 .where(qUser.eq(user))
-                .execute();
+                .fetchOne();
+
+        if (countLevel != null && countLevel < 101) {
+            jq.update(qUser)
+                    .set(qUser.level, qUser.level.add(1))
+                    .where(qUser.eq(user))
+                    .execute();
+        }
     }
 }
