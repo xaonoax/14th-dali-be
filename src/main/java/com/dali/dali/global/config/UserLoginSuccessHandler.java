5ed36dc3cc3f086a4377f506a8c6953ee9d072bc
jjaei <ieajnowk@gmail.com>
2023-09-06 04:05:23 +0900

    Chore: 배포 주소로 수정

--- src/main/java/com/dali/dali/global/config/UserLoginSuccessHandler.java
+++ src/main/java/com/dali/dali/global/config/UserLoginSuccessHandler.java
@@ -13,6 +13,6 @@ public class UserLoginSuccessHandler implements AuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        response.sendRedirect("http://ec2-43-201-22-246.ap-northeast-2.compute.amazonaws.com:8080/users/admin");
+        response.sendRedirect("http://ec2-3-39-12-81.ap-northeast-2.compute.amazonaws.com:8080/users/admin");
     }
 }
